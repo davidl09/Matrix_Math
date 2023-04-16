b577d64ef50a25f132a7361266a82003f5ba1bdf
dalae <dalaeer@gmail.com>
2023-04-16 00:38:30 -0400

    Initial commit

---
+++ matrix.c
@@ -0,0 +1,5 @@
+//
+// Created by dalae on 2023-04-16.
+//
+
+#include "matrix.h"
---
+++ matrix.h
@@ -0,0 +1,8 @@
+//
+// Created by dalae on 2023-04-16.
+//
+
+#ifndef MATRIX_MATH_MATRIX_H
+#define MATRIX_MATH_MATRIX_H
+
+#endif //MATRIX_MATH_MATRIX_H
